db093e1ce3458fd427ac01d7509bd64549ca82ed
Vera Fedorova <HighSkillGirl@yandex.com>
2026-06-01 19:36:17 +0300

    micronaut

---
+++ src/main/java/high/skill/girl/project/calories_counter/MainApplication.java
@@ -0,0 +1,9 @@
+package high.skill.girl.project.calories_counter;
+
+import io.micronaut.runtime.Micronaut;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Micronaut.run(MainApplication.class, args);
+    }
+}
---
+++ src/test/java/high/skill/girl/project/calories_counter/CaloriesCounterTest.java
@@ -0,0 +1,22 @@
+package high.skill.girl.project.calories_counter;
+
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class CaloriesCounterTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
